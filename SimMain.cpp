bc2ae66ccb7934afed33e942c9fe2103eacc0ba8
Volkan Aydıngül <volkanaydingul@gmail.com>
2020-09-18 22:27:36 +0300

    Plotting optimization

--- SimMain.cpp
+++ SimMain.cpp
@@ -5,9 +5,9 @@ int main()
 {
 
     std::system("python deleteCsv.py");
-    float dt = 0.01;
+    float dt = 1e-2;
     float startTime = 0.0;
-    float finalTime = 100.0;
+    float finalTime = 10.0;
     int numberOfDrones = 3;
     float initialDistBtwDrns = 2.0;
     Sim *sim = new Sim(dt, startTime, finalTime, numberOfDrones, initialDistBtwDrns);
--- plotDrones.m
+++ plotDrones.m
@@ -1,40 +1,43 @@
-clear;clc;close all;
+clear; clc; close all;
 files = dir("drones");
-k=1;
+k = 1;
+
 for file = files'
+
     if (~file.isdir)
-    fname = ['drones/' file.name];
-    datas(:,:,k) = importDrones(fname);
-    k = k + 1;
+        fname = ['drones/' file.name];
+        datas(:, :, k) = importDrones(fname);
+        k = k + 1;
     end
-    
+
 end
 
 sz = size(datas);
 numberOfDrones = sz(3);
 iter = sz(1);
+v = VideoWriter('newfile','MPEG-4');
+v.Quality = 50;
+open(v)
+a = figure('Visible', 'off');
+hold on;
 
+for k = 1:iter
 
-    a = figure('Visible', 'off');
-    hold on;
-
-    for k = 1:iter
-
-        for m = 1:numberOfDrones
-            plot(datas(k,1,m), datas(k,2,m), 'o');
-        end
-    
-        M(k) = getframe;
-        axis equal
-
-        drawnow;
-        
-        cla;
-        
-
+    for m = 1:numberOfDrones
+        plot(datas(k, 1, m), datas(k, 2, m), 'o');
     end
 
-    hold off;
+    frame = getframe(gcf);
+    writeVideo(v,frame);
+    axis equal
 
-figure;
-movie(M);
+    drawnow;
+
+    
+
+end
+
+hold off;
+
+close(v)
+
